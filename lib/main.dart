a18ecd947319fe23e393e029f7b098cf2fa1eb11
lolopindik <dimaluka2014@gmail.com>
2025-03-07 12:50:19 +0300

    turn on observer

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:rick_test/constrains/preferences.dart';
+import 'package:rick_test/logic/bloc/bloc_observer.dart';
 import 'package:rick_test/presentation/router/app_router.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 void main() {
+  Bloc.observer = AppBlocObserver();
   runApp(const MyApp());
 }
 
